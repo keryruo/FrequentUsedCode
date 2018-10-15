10719ae7d96601c8340bdfafa8305bdd0b53c677
keryruo <keryruo@gmail.com>
2018-10-15 10:44:24 +0800

    Update and rename chrFastaFile.python to chrFastaFile.py

--- chrFastaFile.py
+++ chrFastaFile.py
@@ -6,17 +6,16 @@ def chrFasta(fastaFile,writeDir):
     # 1:22+"X"+"Y"
     chrom_list=[[str(i) for i in xrange(1,23,1)],'X','Y','M']
     chrom = list(chain(*chrom_list))
-    # move to the writing directory
-    os.system('cd {writeDir}'.format(writeDir=writeDir))
     with open(fastaFile,'r') as f:
         for line in f:
             for num in chrom:
                 chrom_num=str(num)
                 if line.startswith(">chr{chrom_num} {chrom_num}".format(chrom_num=chrom_num)):
-                    w=open('chr{chrom_num}.fa'.format(chrom_num=chrom_num),'w')
+                    w=open('{writeDir}/chr{chrom_num}.fa'.format(writeDir=writeDir,chrom_num=chrom_num),'w')
             w.write('{line}'.format(line=line))
             w.flush()
     w.close()
+
 
 # parametes setting and script running
 fastaFile="/mnt/xdlab/ref/GRCh37/GRCh37.p13.genome.fa"
